762dcf3ce1cea437b35a4ef3d1a7f3b80ed67de8
kmvucd <vimarish.kuttapalayammani@ucdconnect.ie>
2025-05-29 10:51:06 +0100

    Updated main.yaml file with rep

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'xray',
     version= '0.0.0',
-    author= 'Bappy iNeuron',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'Vimarish K M',
+    author_email= 'vimarish18100@gmail.com',
     packages= find_packages(),
     install_requires = []
 
